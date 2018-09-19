5da7a282edd2f6ac39e7c3ed6c9d3c06761c31aa
Marc <marc.castillo@berkeley.edu>
2018-09-19 13:05:39 -0700

    done

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,10 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    b = a.map {|elem| elem.to_i + 2}
+    b.uniq!
+    b.select! {|elem| elem % 2 == 0 && elem < 10}
+    return b.sum
+
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,20 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  c = a + b
+  d = c**2
+  return d
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  return a.sort!.map {|elem| elem + 1}
 
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  return "#{first_name} #{last_name}"
 
 end
 
@@ -46,7 +51,32 @@ def scrabble(word)
     w: 4,
     x: 8,
     y: 4,
-    z: 10,
+    z: 10
   }
   # Q5 CODE HERE
+  a = word.chars
+  b = Array.new
+  a.each do |elem|
+    c = values[:"#{elem}"]
+    b.push(c)
+  end
+  return b.sum
+
+end
+
+if __FILE__ == $0
+  q1 = squared_sum(2,3)
+  puts q1
+
+  q2 = sort_array_plus_one([3, 1, 2])
+  puts q2
+
+  q3 = combine_name("Ethan", "Lee")
+  puts q3
+
+  q4 = blockin_time(['1', '2', '3', '4', '5', '10'])
+  puts q4
+
+  q5 = scrabble("equestrian")
+  puts q5
 end
